325f20e31e5d36e877238aafa9ae748d314d05d1
ValenPettaz1 <vpettazi@udesa.edu.ar>
2023-08-15 11:52:43 -0300

    City OK

--- point.hs
+++ point.hs
@@ -1,5 +1,5 @@
-module Point ( Point, newP, difP)
-   where
+module Point ( Point, newP, difP) 
+    where
 
 data Point = Poi Int Int deriving (Eq, Show)
 
@@ -8,5 +8,3 @@ newP = Poi
 
 difP :: Point -> Point -> Float  -- distancia absoluta
 difP (Poi x1 y1) (Poi x2 y2) = sqrt (fromIntegral(x1-x2) ** 2 + fromIntegral(y1-y2) ** 2)
-
-
