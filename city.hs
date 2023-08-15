356bda4df5d82d6c72e5a1157507bb036431cffc
ValenPettaz1 <vpettazi@udesa.edu.ar>
2023-08-15 11:52:50 -0300

    City OK

--- city.hs
+++ city.hs
@@ -1,8 +1,15 @@
 module City ( City, newC, nameC, distanceC )
    where
 
+import Point
+
 data City = Cit String Point deriving (Eq, Show)
 
 newC :: String -> Point -> City
+newC = Cit 
+
 nameC :: City -> String
+nameC (Cit name _) = name
+
 distanceC :: City -> City -> Float
+distanceC (Cit _ loc1) (Cit _ loc2) = difP loc1 loc2
