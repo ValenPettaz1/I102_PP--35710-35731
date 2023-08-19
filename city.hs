f72ae1cc1419a9dfbb649d8a4858560e8dabbc3d
ValenPettaz1 <vpettazi@udesa.edu.ar>
2023-08-19 14:27:58 -0300

    tests para city.hs. Test OK

--- city.hs
+++ city.hs
@@ -1,7 +1,7 @@
 module City ( City, newC, nameC, distanceC )
    where
 
-import Point ( Point, difP )
+import Point  
 
 data City = Cit String Point deriving (Eq, Show)
 
@@ -13,4 +13,12 @@ nameC (Cit name _) = name
 
 distanceC :: City -> City -> Float
 distanceC (Cit _ loc1) (Cit _ loc2) = difP loc1 loc2
+
+sannicolas = newC "San Nicolas" (newP 100 250)
+sarmiento = newC "Sarmiento" (newP (-150) 250)
+
+test = [distanceC sarmiento sannicolas == 250.0,
+        distanceC sannicolas sarmiento == 250.0,
+        nameC sarmiento == "Sarmiento",
+        True]
 
