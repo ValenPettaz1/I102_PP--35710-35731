c721f85e97018058385786bbb89c50f6b6ac762c
ValenPettaz1 <vpettazi@udesa.edu.ar>
2023-08-19 13:41:49 -0300

    Corrección de nombres  y funciones to main
    Co-authored-by: JuanLaszeskiUdeSA <JuanLaszeskiUdeSA@users.noreply.github.com>

--- link.hs
+++ link.hs
@@ -1,4 +1,4 @@
-module Link ( Link, newL, listCitiesL, linksL, connectsL, capacityL, delayL )
+module Link ( Link, newL, linksL, connectsL, capacityL, delayL )
    where
 
 import City ( City )
@@ -9,14 +9,14 @@ data Link = Lin City City Quality deriving (Eq, Show)
 newL :: City -> City -> Quality -> Link -- genera un link entre dos ciudades distintas
 newL = Lin
 
-listCitiesL :: Link -> [City]
-listCitiesL (Lin city1 city2 _) = [city1, city2]
+getCitiesL :: Link -> [City]
+getCitiesL (Lin city1 city2 _) = [city1, city2]
 
 connectsL :: City -> Link -> Bool   -- indica si esta ciudad es parte de este link
-connectsL city link = city `elem` listCitiesL link
+connectsL city link = city `elem` getCitiesL link
 
 linksL :: City -> City -> Link -> Bool -- indica si estas dos ciudades distintas estan conectadas mediante este link
-linksL city1 city2 link = city1 `elem` listCitiesL link && city2 `elem` listCitiesL link
+linksL city1 city2 link = city1 `elem` getCitiesL link && city2 `elem` getCitiesL link
 
 capacityL :: Link -> Int
 capacityL (Lin _ _ qlty) = capacityQ qlty
--- tunel.hs
+++ tunel.hs
@@ -1,4 +1,4 @@
-module Tunel ( Tunel, newT, listLinksT, connectsT, usesT, delayT )
+module Tunel ( Tunel, newT, connectsT, usesT, delayT )
    where
 
 import Link
@@ -9,16 +9,16 @@ data Tunel = Tun [Link] deriving (Eq, Show)
 newT :: [Link] -> Tunel
 newT = Tun 
 
-listLinksT :: Tunel -> [Link]
-listLinksT (Tun link) = listCitiesL link
+getLinksT :: Tunel -> [Link]
+getLinksT (Tun link) = listCitiesL link
 
 connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
-connectsT city1 city2 tunel = listLinksT `elem` listLinksT tunel
+connectsT city1 city2 tunel = getLinksT `elem` getLinksT tunel
 
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
-usesT link tunel = link `elem` listLinksT
+usesT link tunel = link `elem` getLinksT
 
 
 --delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
---delayT tunel = listLinksT tunel
+--delayT tunel = getLinksT tunel
 
