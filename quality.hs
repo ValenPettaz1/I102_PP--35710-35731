0f3198c1e2a8595179dfd8ae25910ffe72fcabcd
ValenPettaz1 <vpettazi@udesa.edu.ar>
2023-08-21 20:17:15 -0300

    Agregamos guardas para verificar entradas
    Co-authored-by: JuanLaszeskiUdeSA <JuanLaszeskiUdeSA@users.noreply.github.com>

--- quality.hs
+++ quality.hs
@@ -4,7 +4,11 @@ module Quality ( Quality, newQ, capacityQ, delayQ)
 data Quality = Qua String Int Float deriving (Eq, Show)
 
 newQ :: String -> Int -> Float -> Quality
-newQ = Qua
+newQ name capacity delay | capacity > 0 && delay > 0 && name /= "" = Qua name capacity delay
+                         | capacity < 0 = error "La capacidad debe ser mayor a 0"
+                         | delay < 0 = error "El delay debe ser mayor a 0"
+                         | name == "" = error "Debe poner un nombre"
+                         | otherwise = error "Calidad inválida"
 
 capacityQ :: Quality -> Int -- cuantos túneles puede tolerar esta conexión
 capacityQ (Qua _ capacity _) = capacity
@@ -12,11 +16,17 @@ capacityQ (Qua _ capacity _) = capacity
 delayQ :: Quality -> Float  -- la demora por unidad de distancia que sucede en las conexiones de este canal
 delayQ (Qua _ _ delay) = delay
 
+
 quality1 = newQ "Baja" 3 0.1
 quality2 = newQ "Media" 7 0.05
 quality3 = newQ "Alta" 10 0.01
 
-
 testQ = [capacityQ quality1 == 3,
         delayQ quality1 == 0.1, 
         True] 
+
+invQuality1 = newQ "Baja" (-3) 0.1
+invQuality2 = newQ "" 3 0.1
+invQuality3 = newQ "Media" 3 (-0.1)
+invQuality4 = newQ "" (-3) (-0.1)
+
